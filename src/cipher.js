1520142f3f6e8a2e261a974664f48dd3517f945c
Andrea0611 <andrea.parquiroz@gmail.com>
2019-01-16 17:54:56 -0600

    Realice la función de code, realice la estructura de HTML

--- src/cipher.js
+++ src/cipher.js
@@ -1,3 +1,36 @@
 window.cipher = {
   // ... 
+  
+ encode: (desplazamiento, mensajeIngreso) => {
+  let i=0
+  let menCifrado = "";
+   
+
+   for (i=0; i<mensajeIngreso.length; i++){
+     let mensAscii = mensajeIngreso.toUpperCase().charCodeAt(i);
+     let formula = (mensAscii - 65 + parseInt(desplazamiento)) % 26 + 65;
+     let resultado = String.fromCharCode(formula);
+     menCifrado += resultado;
+
+     
+     
+    }
+    console.log(menCifrado)
+   return menCifrado
+
+ },
+ 
+ decode: (desplazamientoCodificado, mensajeIngresoDecodificado) => {
+
+  let menDesifrado= " ";
+
+  for (let i=0; i<mensajeIngresoDecodificado.length; i++);
+   let mensAsciiDeco = mensajeIngresoDecodificado.toUpperCase().charCodeAt();
+   let formulaDeco = (mensAsciiDeco - 65 + parseInt(desplazamientoCodificado)) % 26 + 65;
+   let resultadoDeco = String.fromCharCode(formulaDeco);
+   menDesifrado += resultadoDeco;
+   
+   return menDesifrado
+ }
+
 };
---
+++ src/index.js
@@ -0,0 +1,16 @@
+// onclick que guarda el mensaje y el offset del boton de codificación
+document.getElementById("codificar").addEventListener("click", () => {
+let mensajeIngreso = document.getElementById("mensaje").value;
+let desplazamiento = document.getElementById ("offset").value;
+window.cipher.encode (desplazamiento, mensajeIngreso);
+console.log (window.cipher.encode (desplazamiento, mensajeIngreso))
+
+})
+
+// onclick que guarda el mensaje y el offset del boton de decodificación
+document.getElementById("decodificar").addEventListener ("click", () => {
+let mensajeIngresoCodificado = document.getElementById ("mensaje").value;
+let desplazamientoCodificado = document.getElementById ("offset").value;
+window.cipher.decode (desplazamientoCodificado, mensajeIngresoDecodificado);
+
+})
