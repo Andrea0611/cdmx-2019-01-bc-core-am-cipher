c11477edff621fb4137d42a75e5f63ad9562a7e2
Andrea0611 <andrea.parquiroz@gmail.com>
2019-01-17 11:23:31 -0600

    Relice los test, y corregi los errores que me marcaban, ahora mi código esta sin errores, además puse la ventana que pinta mis resultados de los mensajes codificados y decodificados

--- src/cipher.js
+++ src/cipher.js
@@ -1,36 +1,37 @@
 window.cipher = {
   // ... 
-  
- encode: (desplazamiento, mensajeIngreso) => {
-  let i=0
-  let menCifrado = "";
-   
 
-   for (i=0; i<mensajeIngreso.length; i++){
-     let mensAscii = mensajeIngreso.toUpperCase().charCodeAt(i);
-     let formula = (mensAscii - 65 + parseInt(desplazamiento)) % 26 + 65;
-     let resultado = String.fromCharCode(formula);
-     menCifrado += resultado;
+  encode: (desplazamiento, mensajeIngreso) => {
+    let i = 0
+    let menCifrado = "";
 
-     
-     
+
+    for (i = 0; i < mensajeIngreso.length; i++) {
+      let mensAscii = mensajeIngreso.toUpperCase().charCodeAt(i);
+      let formula = (mensAscii - 65 + parseInt(desplazamiento)) % 26 + 65;
+      let resultado = String.fromCharCode(formula);
+      menCifrado += resultado;
+
+
+
     }
-    console.log(menCifrado)
-   return menCifrado
+    //console.log(menCifrado)
+    return menCifrado
 
- },
- 
- decode: (desplazamientoCodificado, mensajeIngresoDecodificado) => {
+  },
 
-  let menDesifrado= " ";
+  decode: (desplazamientoCodificado, mensajeIngresoCodificado) => {
+    let i = 0
+    let mensajeDecifrado = "";
 
-  for (let i=0; i<mensajeIngresoDecodificado.length; i++);
-   let mensAsciiDeco = mensajeIngresoDecodificado.toUpperCase().charCodeAt();
-   let formulaDeco = (mensAsciiDeco - 65 + parseInt(desplazamientoCodificado)) % 26 + 65;
-   let resultadoDeco = String.fromCharCode(formulaDeco);
-   menDesifrado += resultadoDeco;
-   
-   return menDesifrado
- }
+    for (i = 0; i < mensajeIngresoCodificado.length; i++) {
+      let mensAsciiDeco = mensajeIngresoCodificado.toUpperCase().charCodeAt(i);
+      let formulaDeco = (mensAsciiDeco + 65 - parseInt(desplazamientoCodificado)) % 26 + 65;
+      let resultadoDeco = String.fromCharCode(formulaDeco);
+      mensajeDecifrado += resultadoDeco;
+    }
+    //console.log (mensajeDecifrado)
+    return mensajeDecifrado
+  }
+}
 
-};
